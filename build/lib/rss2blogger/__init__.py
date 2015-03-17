e4ef9340f0d01b7b7bea2610c41a4ce61a427a66
finkelstein <finkelstein@adelphi.edu>
2015-03-17 14:29:17 -0400

    Allowing user to pass url string to RSS2Blogger constructor.

--- build/lib/rss2blogger/__init__.py
+++ build/lib/rss2blogger/__init__.py
@@ -3,8 +3,11 @@ import urllib2
 import feedparser
 
 class RSS2Blogger():
-	def display_info(self):
-		f=feedparser.parse('http://adelphi-tech.blogspot.com/feeds/posts/default')
+	def __init__(self, url=None):
+		self.url=url
+
+	def display(self):
+		f=feedparser.parse(self.url)
 	
 		for entry in f.entries:
 			print entry.title+' - '+entry.link+' - '+entry.published+"\n"
--- rss2blogger/__init__.py
+++ rss2blogger/__init__.py
@@ -3,8 +3,11 @@ import urllib2
 import feedparser
 
 class RSS2Blogger():
+	def __init__(self, url=None):
+		self.url=url
+
 	def display(self):
-		f=feedparser.parse('http://adelphi-tech.blogspot.com/feeds/posts/default')
+		f=feedparser.parse(self.url)
 	
 		for entry in f.entries:
 			print entry.title+' - '+entry.link+' - '+entry.published+"\n"
